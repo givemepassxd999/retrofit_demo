a7e11bacb63cfbe287795b87864b6d0f6c543d3e
ARK88\Rick.Wu <rick.wu@ark88.net>
2018-08-08 20:44:56 +0800

    調整retrofit為singleton

--- app/src/main/java/tw/com/test/retrofitdemo/MainActivity.java
+++ app/src/main/java/tw/com/test/retrofitdemo/MainActivity.java
@@ -14,15 +14,13 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 
 public class MainActivity extends AppCompatActivity {
-    private AppClient appClient;
     private Retrofit retrofit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final TextView tv = findViewById(R.id.info);
-        appClient = new AppClient();
-        retrofit = appClient.getClient();
+        retrofit = AppClientManager.getClient();
         Button info = findViewById(R.id.send_data);
         info.setOnClickListener(new View.OnClickListener() {
             @Override
