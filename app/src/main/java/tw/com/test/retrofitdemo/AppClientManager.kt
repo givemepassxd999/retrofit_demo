582a932db0e16ae148bbf7a25d700d48a35412e5
Rick <givemepassxd999@gmail.com>
2019-07-28 16:00:17 +0800

    add interceptor message

--- app/src/main/java/tw/com/test/retrofitdemo/AppClientManager.kt
+++ app/src/main/java/tw/com/test/retrofitdemo/AppClientManager.kt
@@ -1,14 +1,25 @@
 package tw.com.test.retrofitdemo
 
+import android.util.Log
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
+
 class AppClientManager private constructor() {
     private val retrofit: Retrofit
-    private val okHttpClient = OkHttpClient()
+    private var logging = HttpLoggingInterceptor(object : HttpLoggingInterceptor.Logger {
+        override fun log(message: String) {
+            Log.i("interceptor msg", message)
+        }
+    })
+
+    private var okHttpClient : OkHttpClient
 
     init {
+        logging.level = HttpLoggingInterceptor.Level.BODY
+        okHttpClient = OkHttpClient().newBuilder().addInterceptor(logging).build()
         retrofit = Retrofit.Builder()
                 .baseUrl(Config.URL)
                 .addConverterFactory(GsonConverterFactory.create())
@@ -22,3 +33,4 @@ class AppClientManager private constructor() {
             get() = manager.retrofit
     }
 }
+
